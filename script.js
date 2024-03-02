73f7d95b3a112c80e4b5501bfb806782289723d2
ermheld <161643169+ermheld@users.noreply.github.com>
2024-03-02 00:01:19 +0000

    Update script.js

--- script.js
+++ script.js
@@ -388,7 +388,6 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 initialize3DModel(); // Call the function to initialize and load the 3D model
 });
-
 function initialize3DModel() {
     var scene = new THREE.Scene();
     var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
